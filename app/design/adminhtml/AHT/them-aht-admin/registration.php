95dcc1f38500530f4ca6420c14d624d5d8cd7409
Hoang Phuong Quy <quyhp@talent.ominext.com>
2020-04-20 21:28:09 +0700

    new code

---
+++ app/design/adminhtml/AHT/them-aht-admin/registration.php
@@ -0,0 +1,4 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'adminhtml/AHT/theme-aht-admin', __DIR__); // Example: 'adminhtml/Magento/backend'
