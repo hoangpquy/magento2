d283218c04e844ce70d9091d4651a5a725e80584
Hoang Phuong Quy <quyhp@talent.ominext.com>
2020-04-22 17:58:14 +0700

    report 22/4/2020 13h30

--- app/code/Unit1/Test2/registration.php
+++ app/code/Unit1/Test2/registration.php
@@ -1,10 +1,10 @@
 <?php
-/**
-*
-* Copyright © Magento. All rights reserved.
-* See COPYING.txt for license details.
-*/
+    /**
+    *
+    * Copyright © Magento. All rights reserved.
+    * See COPYING.txt for license details.
+    */
     \Magento\Framework\Component\ComponentRegistrar::register( 
     \Magento\Framework\Component\ComponentRegistrar::MODULE, 
     'Unit1_Test2', __DIR__
-    );
+);
